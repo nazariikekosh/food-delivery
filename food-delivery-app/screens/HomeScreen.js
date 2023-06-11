fd7146cdb1773f7fe13db7dc077a15022fc99264
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-11 17:11:50 +0300

    add searchBar

--- food-delivery-app/screens/HomeScreen.js
+++ food-delivery-app/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle="dark-content" />
+      {/* search bar*/}
+      <View className="flex-row items-center space-x-2 px-4 py-2">
+        <View className="flex-row flex-1 item-center p-3 rounded-full border border-grey-300"></View>
+      </View>
     </SafeAreaView>
   )
 }
