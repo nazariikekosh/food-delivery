50ccd30330fc3ab869e36d87304e7b7566c34d00
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-11 16:34:35 +0300

    add safe Area View for HomeScreen

--- food-delivery-app/navigation.js
+++ food-delivery-app/navigation.js
@@ -9,7 +9,9 @@ import RestaurantScreen from './screens/RestaurantScreen';
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
