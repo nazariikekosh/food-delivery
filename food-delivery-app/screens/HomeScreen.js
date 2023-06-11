d71c4592b703589936632117dbac70d2ae5ae8b7
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-11 18:01:06 +0300

    add maps icon

--- food-delivery-app/screens/HomeScreen.js
+++ food-delivery-app/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
@@ -10,8 +10,12 @@ export default function HomeScreen() {
       <StatusBar barStyle="dark-content" />
       {/* search bar*/}
       <View className="flex-row items-center space-x-2 px-4 pb-2">
-        <View className="flex-row flex-1 item-center p-3 rounded-full border border-grey-300">
-          <Icon.Search height="25" width="25" stroke="grey" />
+        <View className="flex-row flex-1 item-center p-3 rounded-full border border-gray-300">
+          <Icon.Search height="25" width="25" stroke="gray" />
+          <TextInput placeholder='Restaurants' className="ml-2 flex-1" />
+          <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-grey-300">
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+          </View>
         </View>
       </View>
     </SafeAreaView>
