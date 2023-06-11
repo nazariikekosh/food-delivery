00994c9c0089b8f45bf5b8a96ae4e438eba0c886
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-11 18:11:07 +0300

    seachBar creat finish

--- food-delivery-app/screens/HomeScreen.js
+++ food-delivery-app/screens/HomeScreen.js
@@ -13,8 +13,9 @@ export default function HomeScreen() {
         <View className="flex-row flex-1 item-center p-3 rounded-full border border-gray-300">
           <Icon.Search height="25" width="25" stroke="gray" />
           <TextInput placeholder='Restaurants' className="ml-2 flex-1" />
-          <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-grey-300">
+          <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
             <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text className="text-gray-600">Drohobuch, UK</Text>
           </View>
         </View>
       </View>
