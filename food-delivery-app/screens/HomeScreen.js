1cd5c491407f4df675a0dfa145ad40556567d7f1
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-11 17:31:30 +0300

    install and add icon to search

--- food-delivery-app/screens/HomeScreen.js
+++ food-delivery-app/screens/HomeScreen.js
@@ -2,14 +2,17 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
+import * as Icon from "react-native-feather"
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="bg-white">
       <StatusBar barStyle="dark-content" />
       {/* search bar*/}
-      <View className="flex-row items-center space-x-2 px-4 py-2">
-        <View className="flex-row flex-1 item-center p-3 rounded-full border border-grey-300"></View>
+      <View className="flex-row items-center space-x-2 px-4 pb-2">
+        <View className="flex-row flex-1 item-center p-3 rounded-full border border-grey-300">
+          <Icon.Search height="25" width="25" stroke="grey" />
+        </View>
       </View>
     </SafeAreaView>
   )
