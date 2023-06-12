97912d17d24c03dc95e2e82faa28abe19aceb580
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-12 18:06:05 +0300

    add stars to cart

--- food-delivery-app/components/restaurantCard.js
+++ food-delivery-app/components/restaurantCard.js
@@ -7,7 +7,13 @@ export default function RestaurantCard({item}) {
         <View className="mr-6 bg-white rounded-3xl shadow-lg">
             <Image className="h-36 w-64 rounded-t-3xl" source={item.image} />
             <View className="px-3 pb-4 space-y-2">
-                <Text>{item.name}</Text>
+                <Text className="text-lg font-bold pt-2">{item.name}</Text>
+                <View className="flex-row items-center space-x-1">
+                    <Image source={require('../assets/images/fullStar.png')} className="h-4 w-4" />
+                    <Text className="text-xs">
+                        <Text className="text-green-700">{item.stars}</Text>
+                    </Text>
+                </View>
             </View>
         </View>
     </TouchableWithoutFeedback>
