39833d22d3d4b034dcd386b5b5421fbbef9b465a
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-12 14:43:29 +0300

    add activeCategory setActiveCat...

--- food-delivery-app/components/categories.js
+++ food-delivery-app/components/categories.js
@@ -1,8 +1,9 @@
 import { View, Text, ScrollView, TouchableOpacity, Image} from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { categories } from '../constants'
 
 export default function Categories() {
+  const [activeCategory, setActiveCategory] = useState(null);
   return (
     <View className="mt-4">
       <ScrollView
@@ -15,14 +16,18 @@ export default function Categories() {
       >
         {
           categories.map((category, index)=>{
+            let isActive = category.id===activeCategory;
+            let btnClass = isActive? ' bg-gray-600 ' : ' bg-gray-200';
+            let textClass = isActive? 'font-semibold text-gray-800' : 'text-gray-500'
             return(
                <View key={index} className="flex justify-center items-center mr-6">
                 <TouchableOpacity
-                  className="p-1 rounded-full shadow bg-gray-200">
+                  onPress={()=> setActiveCategory(category.id)}
+                  className={"p-1 rounded-full shadow bg-gray-200"+btnClass}>
                     <Image style={{width: 45, height: 45}}
                       source={category.image} />
-                    <Text className="text-sm">{category.name}</Text>
                   </TouchableOpacity>
+                    <Text className={"text-sm"+textClass}>{category.name}</Text>
                </View>
             )
           })
