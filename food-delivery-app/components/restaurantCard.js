7b2ff8dfbef0319d17c522ad5d826162e49eba26
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-13 11:51:37 +0300

    create restaurant screens add image

--- food-delivery-app/components/restaurantCard.js
+++ food-delivery-app/components/restaurantCard.js
@@ -1,11 +1,17 @@
 import { View, Text, TouchableNativeFeedback, TouchableWithoutFeedback, Image} from 'react-native'
 import React from 'react'
 import * as Icon from "react-native-feather"
+import { themeColors } from '../theme'
+import { useNavigation } from '@react-navigation/native'
 
 export default function RestaurantCard({item}) {
+    const navigation = useNavigation();
   return (
-    <TouchableWithoutFeedback>
-        <View className="mr-6 bg-white rounded-3xl shadow-lg">
+    <TouchableWithoutFeedback onPress={()=> navigation.navigate('Restaurant',{...item})}>
+        <View style={{
+            shadowColor: themeColors.bgColor(0.2),
+            shadowRadius: 7
+        }} className="mr-6 bg-white rounded-3xl shadow-lg">
             <Image className="h-36 w-64 rounded-t-3xl" source={item.image} />
             <View className="px-3 pb-4 space-y-2">
                 <Text className="text-lg font-bold pt-2">{item.name}</Text>
@@ -20,6 +26,7 @@ export default function RestaurantCard({item}) {
                 </View>
                 <View className="flex-row items-center space-x-1">
                     <Icon.MapPin color="gray" width="15" height="15" />
+                    <Text className="text-gray-700 text-xs">Nearby. {item.address}</Text>
                 </View>
             </View>
         </View>
--- food-delivery-app/screens/RestaurantScreen.js
+++ food-delivery-app/screens/RestaurantScreen.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
 
 export default function RestaurantScreen() {
+  const {params} = useRoute();
+  let item = params;
+  // console.log('restaurant:', item);
   return (
     <View>
-      <Text>RestaurantScreen</Text>
+      <ScrollView>
+        <View className="relative">
+          <Image className="w-full h-72" source={item.image} />
+        </View>
+      </ScrollView>
     </View>
   )
 }
