95d26d22100b47b8aeb1e8f4f7403c554a45ea3e
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-13 13:03:43 +0300

    add navigation goBack to ResScr

--- food-delivery-app/screens/RestaurantScreen.js
+++ food-delivery-app/screens/RestaurantScreen.js
@@ -1,18 +1,33 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native'
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import * as Icon from "react-native-feather";
+import { themeColors } from "../theme";
 
 export default function RestaurantScreen() {
-  const {params} = useRoute();
+  const { params } = useRoute();
+  const navigation = useNavigation();
   let item = params;
   // console.log('restaurant:', item);
   return (
     <View>
-      <ScrollView>
-        <View className="relative">
-          <Image className="w-full h-72" source={item.image} />
-        </View>
-      </ScrollView>
-    </View>
-  )
+    <ScrollView>
+      <View className="relative">
+        <Image className="w-full h-72" source={item.image} />
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="absolute top-14 left-4 bg-gray-50 p-2  rounded-full shadow"
+        >
+          <Icon.ArrowLeft strokeWidth={3} stroke={themeColors.bgColor(1)} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  </View>
+  );
 }
