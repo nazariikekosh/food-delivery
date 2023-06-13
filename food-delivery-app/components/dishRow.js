1cd17db50157217c4c0ebb0d8e72d1b9bae26007
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-13 14:39:26 +0300

    add dish Row descr and price

--- food-delivery-app/components/dishRow.js
+++ food-delivery-app/components/dishRow.js
@@ -9,6 +9,12 @@ export default function DishRow({item}) {
         <View className="flex flex-1 space-y-3">
             <View className="pl-3">
                 <Text className="text-xl">{item.name}</Text>
+                <Text className="text-gray-700">{item.description}</Text>
+            </View>
+            <View className="flex-row justify-between pl-3 items-center">
+                <Text className="text-gray-700 text-lg font-bold">
+                   $ {item.price}
+                </Text>
             </View>
         </View>
     </View>
