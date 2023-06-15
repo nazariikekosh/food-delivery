bf20d21ebfafdccd4bff44511d2e49549f17414b
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-15 11:50:48 +0300

    delivery-screen add descript delivery

--- food-delivery-app/screens/DeliveryScreen.js
+++ food-delivery-app/screens/DeliveryScreen.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { featured } from '../constants'
 import { useNavigation } from '@react-navigation/native';
 import MapView, {Marker} from 'react-native-maps'
+import { themeColors } from '../theme';
 
 export default function DeliveryScreen() {
     const restaurant = featured.restaurants[0];
@@ -25,8 +26,26 @@ export default function DeliveryScreen() {
                         latitude: restaurant.lat,
                         longitude: restaurant.lng,
                     }}
+                    title={restaurant.name}
+                    description={restaurant.description}
+                    pinColor={themeColors.bgColor(1)}
                 />
             </MapView>
+            <View className="rounded-t-3xl -mt-12 bg-white relative">
+                <View className="flex-row justify-between px-5 pt-10">
+                    <View>
+                        <Text className="text-lg text-gray-700 font-semibold">
+                            Estimated Arrival
+                        </Text>
+                        <Text className="text-3xl font-extrabold text-gray-700">
+                            20-30 Minutes
+                        </Text>
+                        <Text className="mt-2 text-gray-700 font-semibold">
+                            You order is own its way!
+                        </Text>
+                    </View>
+                </View>
+            </View>
     </View>
   )
 }
