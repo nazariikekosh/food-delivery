90ea47a1469104fe8414444a0cf4751a60b367e3
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-15 10:02:34 +0300

    add place order gif

--- food-delivery-app/screens/PreparingOrderScreen.js
+++ food-delivery-app/screens/PreparingOrderScreen.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 export default function PreparingOrderScreen() {
   return (
-    <View>
-      <Text>OrderPrepairing</Text>
+    <View className="flex-1 bg-white justify-center items-center">
+      <Image source={require('../assets/images/delivery.gif')} className="h-80 w-80" />
     </View>
   )
 }
