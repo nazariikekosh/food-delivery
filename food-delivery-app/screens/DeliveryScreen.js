0995bd8b228f94fe77f64d6ceaa0903d94a0160a
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-15 20:53:57 +0300

    curjer info add

--- food-delivery-app/screens/DeliveryScreen.js
+++ food-delivery-app/screens/DeliveryScreen.js
@@ -1,9 +1,10 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { featured } from '../constants'
 import { useNavigation } from '@react-navigation/native';
 import MapView, {Marker} from 'react-native-maps'
 import { themeColors } from '../theme';
+import * as Icon from "react-native-feather"
 
 export default function DeliveryScreen() {
     const restaurant = featured.restaurants[0];
@@ -53,7 +54,23 @@ export default function DeliveryScreen() {
                             style={{backgroundColor: 'rgba(255,255,255,0.4)'}}>
                                 <Image className="h-16 w-16 rounded-full"
                                  source={require('../assets/images/deliveryGuy.jpg')} />
-                        </View>         
+                        </View>       
+                        <View className="flex-1 ml-3">
+                            <Text className="text-lg font-bold text-white">
+                                Nazarii Kekosh    
+                            </Text>    
+                            <Text className="font-semibold text-white">
+                                Your Rider   
+                            </Text>  
+                         </View> 
+                         <View className="flex-row items-center space-x-3 mr-3">
+                            <TouchableOpacity className="bg-white p-2 rounded-full">
+                                <Icon.Phone fill={themeColors.bgColor(1)} stroke={themeColors.bgColor(1)} strokeWidth={1} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={()=>navigation.navigate('Home')} className="bg-white p-2 rounded-full">
+                                <Icon.X stroke={'red'} strokeWidth={4} />
+                            </TouchableOpacity>       
+                        </View> 
                 </View>
             </View>
     </View>
